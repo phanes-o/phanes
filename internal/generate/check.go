1560538d542c513a86bdffc7a5fca5f821826119
jack <beanit@163.com>
2023-07-03 09:50:22 +0800

    opt: file path

--- internal/generate/check.go
+++ internal/generate/check.go
@@ -26,37 +26,6 @@ func checkProjectExist(project string) bool {
 	return fileExists(project)
 }
 
-func checkDestination(src, conf *Destinations) *Destinations {
-	if conf.Bll != "" {
-		src.Bll = conf.Bll
-	}
-	if conf.Model != "" {
-		src.Model = conf.Model
-	}
-	if conf.Entity != "" {
-		src.Entity = conf.Entity
-	}
-	if conf.Cache != "" {
-		src.Cache = conf.Cache
-	}
-	if conf.HttpApi != "" {
-		src.HttpApi = conf.HttpApi
-	}
-	if conf.GrpcApi != "" {
-		src.GrpcApi = conf.GrpcApi
-	}
-	if conf.StoreMysql != "" {
-		src.StoreMysql = conf.StoreMysql
-	}
-	if conf.StorePostgres != "" {
-		src.StorePostgres = conf.StorePostgres
-	}
-	if conf.StoreInterface != "" {
-		src.StoreInterface = conf.StoreInterface
-	}
-	return src
-}
-
 func fileExists(filename string) bool {
 	_, err := os.Stat(filename)
 	return !os.IsNotExist(err)
